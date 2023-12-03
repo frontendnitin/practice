916c6a87013b878f8d60b4fd6c95a27f0003c2c6
frontendnitin <nitinchauhan707864@gmail.com>
2023-12-03 18:15:11 +0530

    feat: push from terminal

--- practice1.js
+++ practice1.js
@@ -1,14 +1,10 @@
 // PREMITIVE DATATYPE AND NON PRIMITIVE DATATYPE
-
-
+// change demo for git
 
 const item = {
-   "Nitin":true,
-   "Shubh":false,
-   "lovish":67,
-   "rohan":undefined
-}
- console.log(item["lovish"])
-
-
- 
+  Nitin: true,
+  Shubh: false,
+  lovish: 67,
+  rohan: undefined,
+};
+console.log(item["lovish"]);
